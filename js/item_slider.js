fcd9848e1b23782e05f0abf9953f6d3da9ccde21
skein <genji199544@gmail.com>
2021-03-18 04:26:40 +0900

    modify

---
+++ js/item_slider.js
@@ -0,0 +1,10 @@
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+});
